0b95fe2ab9051b22e1dc075496c6516fcd685e95
KIMURA Tetsuro <haribote.nobody@gmail.com>
2026-07-28 10:42:39 +0900

    feat(nvim): show gitignored files in snacks explorer and files picker

--- .config/nvim/lua/plugins/explorer.lua
+++ .config/nvim/lua/plugins/explorer.lua
@@ -5,9 +5,11 @@ return {
       sources = {
         explorer = {
           hidden = true,
+          ignored = true,
         },
         files = {
           hidden = true,
+          ignored = true,
         },
       },
     },
